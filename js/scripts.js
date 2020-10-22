aefa7e9a5ca6130d58c004a685c63d5cb6da27b2
Amanda Otero <mandystar018@gmail.com>
2020-10-22 16:29:46 -0700

    Add if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
+function findCharacter(answer1, answer2, answer3) {
+  let score = answer1 + answer2 + answer3;
+
+  if (score < 6) {
+    $(".michael").show();
+    
+  } else if (score > 6 && score < 9) {
+    $(".dwight").show();
+
+  } else if (score > 9 && score < 12) {
+    $(".jim").show();
+
+  } else {
+
+  }
+}
+
 $(document).ready(function(){
   $("form#quiz").submit(function(event) {
     event.preventDefault();
-    const input1 =  $("input:radio[name=question1]:checked").val();
-    const input2 =  $("input:radio[name=question2]:checked").val();
-    const input3 =  $("input:radio[name=question3]:checked").val();
+    const input1 =  parseInt($("input:radio[name=question1]:checked").val());
+    const input2 =  parseInt($("input:radio[name=question2]:checked").val());
+    const input3 =  parseInt($("input:radio[name=question3]:checked").val());
 
-      
+    findCharacter(input1, input2, input3);
   });
 });
