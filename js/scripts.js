723599b996ce71df223f58f884bae77ea47a88f6
Amanda Otero <mandystar018@gmail.com>
2020-10-22 16:01:26 -0700

    Add questions and answers, also add logic with branches
    Co-authored-by: Ben McFarland <benrmcfarland@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
+  $("form#quiz").submit(function(event) {
+    event.preventDefault();
+    const input1 =  $("input:radio[name=question1]:checked").val();
+    const input2 =  $("input:radio[name=question2]:checked").val();
+    const input3 =  $("input:radio[name=question3]:checked").val();
 
+      
+  });
 });
